3962b933e219e58eb7b5e7cd820ea5ae29a0b335
Le0nX <cloudmindone@gmail.com>
2019-02-11 13:29:32 +0300

    Initial Commit

---
+++ Tinkoff_chat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tinkoff_chat
+//
+//  Created by Denis Nefedov on 11/02/2019.
+//  Copyright © 2019 X. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
